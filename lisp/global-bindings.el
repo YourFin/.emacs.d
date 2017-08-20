1378169ac632b9fa7dbdcd19e8103a1c4e0e4057
YourFin <d.junkpobox+github@gmail.com>
2017-08-20 18:15:30 -0500

    C-+ -> increase font C-_ -> decrease font in graphical emacs

--- lisp/global-bindings.el
+++ lisp/global-bindings.el
@@ -1,20 +1,41 @@
-(provide 'global-bindings)
-
-
-
-(defun yf-exit-minibuffer-ev-paste-after
+(defun yf-exit-minibuffer-ev-paste-after ()
     "Exits the helm minibuffer and
 pastes after; intended for helm-calcul-expression"
   (interactive)
   (helm-maybe-exit-minibuffer)
   (evil-paste-after))
 
-(defun yf-exit-minibuffer-ev-paste-before
+(defun yf-exit-minibuffer-ev-paste-before ()
     "Exits the helm minibuffer and
 pastes; intended for helm-calcul-expression"
   (interactive)
   (helm-maybe-exit-minibuffer)
   (evil-paste-before))
 
-(define-key helm-calcul-expression-map (kbd "<RET>") 'yf-exit-minibuffer-ev-paste-after)
-(define-key helm-calcul-expression-map (kbd "S-<RET>") 'yf-exit-minibuffer-ev-paste-before)
+;(define-key helm-calcul-expression-map (kbd "<RET>") 'yf-exit-minibuffer-ev-paste-after)
+;(define-key helm-calcul-expression-map (kbd "S-<RET>") 'yf-exit-minibuffer-ev-paste-before)
+
+(defun yf-possible-increase-size ()
+  "Runs text scale decrease if in not terminal mode, otherwise
+falls through"
+  (interactive)
+  (if (not (display-graphic-p))
+      nil
+    (text-scale-increase 1)
+    (redraw-display)))
+(defun yf-possible-decrease-size ()
+  "Runs text-scale-decrease if not in terminal mode, otherwise
+falls through"
+  (interactive)
+  (if (not (display-graphic-p))
+      nil
+    (text-scale-decrease 1)
+    (redraw-display)))
+
+
+(define-key global-map (kbd "C-+") 'yf-possible-increase-size)
+(define-key global-map (kbd "C-_") 'yf-possible-decrease-size)
+;; not too happy about this, but I want to keep consistancy with my terminals
+(define-key undo-tree-map (kbd "C-_") nil)
+
+(provide 'global-bindings)
