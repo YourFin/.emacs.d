337ca2f36dd0993d368fc76495ec875b94af94fa
YourFin <d.junkpobox+github@gmail.com>
2017-08-20 19:03:37 -0500

    added global ycmd mode

--- init.el
+++ init.el
@@ -78,13 +78,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(company-quickhelp-mode t)
+ '(global-ycmd-mode t)
  '(linum-format (quote dynamic))
  '(package-selected-packages
    (quote
-    (company-ycm evil-magit helm-ag company-quickhelp company-jedi auto-package-update helm-smex smex company-ycmd emacs-ycmd python-django company jinja2-mode python-mode avy tangotange-theme use-package tangotango-theme markdown-mode helm evil ace-jump-mode))))
+    (adaptive-wrap ycmd company-ycm evil-magit helm-ag company-quickhelp company-jedi auto-package-update helm-smex smex company-ycmd emacs-ycmd python-django company jinja2-mode python-mode avy tangotange-theme use-package tangotango-theme markdown-mode helm evil ace-jump-mode)))
+ '(projectile-mode t nil (projectile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "ConsolasHacked" :foundry "MS  " :slant normal :weight normal :height 218 :width normal)))))
