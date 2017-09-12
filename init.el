61288776d3eb1b6ddc98edc09f5259dbdc3b93f5
YourFin <d.junkpobox+github@gmail.com>
2017-09-11 19:53:36 -0500

    Removes ' from delimiters in lisps; closes #8

--- init.el
+++ init.el
@@ -50,7 +50,22 @@
 (use-package markdown-mode)
 (use-package smartparens
   :config
+  ;;taken from http://web.archive.org/web/20170912005704/https://github.com/Fuco1/smartparens/issues/286
+  (sp-with-modes sp--lisp-modes
+  ;; disable ', it's the quote character!
+  (sp-local-pair "'" nil :actions nil)
+  ;; also only use the pseudo-quote inside strings where it serve as
+  ;; hyperlink.
+  (sp-local-pair "`" "'" :when '(sp-in-string-p sp-in-comment-p))
+  (sp-local-pair "`" nil
+                 :skip-match (lambda (ms mb me)
+                               (cond
+                                ((equal ms "'")
+                                 (or (sp--org-skip-markup ms mb me)
+                                     (not (sp-point-in-string-or-comment))))
+                                (t (not (sp-point-in-string-or-comment)))))))
   (add-hook 'prog-mode-hook 'smartparens-mode))
+
 (use-package yasnippet
   :config
   (yas-global-mode 1))
