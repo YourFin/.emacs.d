89c8d273347fed720b0fce9f155ea08afc470329
YourFin <d.junkpobox+github@gmail.com>
2017-09-11 19:46:30 -0500

    Fixed swapped yf/kill-to-second and capital letter bindings

--- lisp/evil-custom-reg.el
+++ lisp/evil-custom-reg.el
@@ -12,7 +12,7 @@
     (let ((interprogram-cut-function
 	   (lambda (str)
 	     (yf-sys-clip-set str)
-      ad-do-it)))
+      ad-do-it))))))
 
 (ad-activate 'kill-new)
 
@@ -22,7 +22,7 @@
   :move-point nil
   :repeat nil
   (interactive "<R><x><y>")
-  (let ((yf/kill-to-second nil))
+  (let ((yf/kill-to-second t))
     (evil-yank beg end type register yank-handler))
   )
 (define-key evil-normal-state-map "y" 'yf-y)
@@ -33,7 +33,7 @@
   :move-point nil
   :repeat nil
   (interactive "<R><x><y>")
-  (let ((yf/kill-to-second t))
+  (let ((yf/kill-to-second nil))
     (evil-yank beg end type register yank-handler))
   )
 (define-key evil-normal-state-map "gy" 'yf-gy)
@@ -41,10 +41,11 @@
 
 (evil-define-operator yf-Y (beg end type register yank-handler)
   "Evil-yank-line that goes to clipboard as well as kill ring"
+  :motion evil-line
   :move-point nil
   :repeat nil
-  (interactive "<R><x><y>")
-  (let ((yf/kill-to-second nil))
+  (interactive "<R><x>")
+  (let ((yf/kill-to-second t))
     (evil-yank-line beg end type register yank-handler))
   )
 (define-key evil-normal-state-map "Y" 'yf-Y)
@@ -52,10 +53,11 @@
 
 (evil-define-operator yf-gY (beg end type register yank-handler)
   "Evil-yank-line that dumps to 2nd in kill ring but not clipboard"
+  :motion evil-line
   :move-point nil
   :repeat nil
-  (interactive "<R><x><y>")
-  (let ((yf/kill-to-second t))
+  (interactive "<R><x>")
+  (let ((yf/kill-to-second nil))
     (evil-yank-line beg end type register yank-handler))
   )
 (define-key evil-normal-state-map "gY" 'yf-gY)
@@ -63,16 +65,18 @@
 
 (evil-define-operator yf-x (beg end type register)
   "Evil-delete-char that dumps to 2nd in kill ring by default"
-  (interactive "<R><x><y>")
-  (let ((yf/kill-to-second t))
+  :motion evil-forward-char
+  (interactive "<R><x>")
+  (let ((yf/kill-to-second nil))
     (evil-delete-char beg end type register)))
 (define-key evil-visual-state-map "x" 'yf-x)
 (define-key evil-normal-state-map "x" 'yf-x)
 
 (evil-define-operator yf-X (beg end type register)
   "Evil-delete-backward-char that dumps to 2nd in kill ring by default"
-  (interactive "<R><x><y>")
-  (let ((yf/kill-to-second t))
+  :motion evil-backward-char
+  (interactive "<R><x>")
+  (let ((yf/kill-to-second nil))
     (evil-delete-backward-char beg end type register)))
 (define-key evil-visual-state-map "X" 'yf-X)
 (define-key evil-normal-state-map "X" 'yf-X)
@@ -80,15 +84,16 @@
 (evil-define-operator yf-d (beg end type register yank-handler)
   "Evil-delete that dumps to 2nd in kill ring by default"
   (interactive "<R><x><y>")
-  (let ((yf/kill-to-second t))
+  (let ((yf/kill-to-second nil))
     (evil-delete beg end type register)))
 (define-key evil-visual-state-map "d" 'yf-d)
 (define-key evil-normal-state-map "d" 'yf-d)
 
 (evil-define-operator yf-D (beg end type register yank-handler)
   "Evil-delete-line that dumps to 2nd in kill ring by default"
-  (interactive "<R><x><y>")
-  (let ((yf/kill-to-second t))
+  :motion evil-end-of-line
+  (interactive "<R><x>")
+  (let ((yf/kill-to-second nil))
     (evil-delete-line beg end type register)))
 (define-key evil-visual-state-map "D" 'yf-D)
 (define-key evil-normal-state-map "D" 'yf-D)
@@ -96,15 +101,16 @@
 (evil-define-operator yf-m (beg end type register yank-handler)
   "Evil-delete that sets custom clipboard along with standard kill"
   (interactive "<R><x><y>")
-  (let ((yf/kill-to-second nil))
+  (let ((yf/kill-to-second t))
     (evil-delete beg end type register)))
 (define-key evil-normal-state-map "m" 'yf-m)
 (define-key evil-visual-state-map "m" 'yf-m)
 
 (evil-define-operator yf-M (beg end type register yank-handler)
   "Evil-delete-line that sets custom clipboard along with standard kill"
-  (interactive "<R><x><y>")
-  (let ((yf/kill-to-second nil))
+  :motion evil-end-of-line
+  (interactive "<R><x>")
+  (let ((yf/kill-to-second t))
     (evil-delete-line beg end type register)))
 (define-key evil-normal-state-map "M" 'yf-M)
 (define-key evil-visual-state-map "M" 'yf-M)
@@ -112,7 +118,7 @@
 (evil-define-operator yf-c (beg end type register yank-handler)
   "Evil-change that dumps to 2nd in kill ring but not clipboard"
   (interactive "<R><x><y>")
-  (let ((yf/kill-to-second t))
+  (let ((yf/kill-to-second nil))
     (evil-change beg end type register yank-handler))
   )
 (define-key evil-normal-state-map "c" 'yf-c)
@@ -121,7 +127,7 @@
 (evil-define-operator yf-gc (beg end type register yank-handler)
   "Evil-change that goes to clipboard as well as kill ring"
   (interactive "<R><x><y>")
-  (let ((yf/kill-to-second nil))
+  (let ((yf/kill-to-second t))
     (evil-change beg end type register yank-hander))
   )
 (define-key evil-normal-state-map "gc" 'yf-gc)
@@ -129,8 +135,9 @@
 
 (evil-define-operator yf-C (beg end type register yank-handler)
   "Evil-change-line that dumps to 2nd in kill ring but not clipboard"
-  (interactive "<R><x><y>")
-  (let ((yf/kill-to-second t))
+  :motion evil-end-of-line
+  (interactive "<R><x>")
+  (let ((yf/kill-to-second nil))
     (evil-change-line beg end type register yank-handler))
   )
 (define-key evil-normal-state-map "C" 'yf-C)
@@ -138,8 +145,9 @@
 
 (evil-define-operator yf-gC (beg end type register yank-handler)
   "Evil-change-line that goes to clipboard as well as kill ring"
-  (interactive "<R><x><y>")
-  (let ((yf/kill-to-second nil))
+  :motion evil-end-of-line
+  (interactive "<R><x>")
+  (let ((yf/kill-to-second t))
     (evil-change-line beg end type register yank-hander)))
 
 (define-key evil-normal-state-map "gC" 'yf-gC)
