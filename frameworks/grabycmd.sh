b81455ef6e88fd816a342d511601fd14091ef0f0
YourFin <d.junkpobox+github@gmail.com>
2018-01-21 19:09:37 -0600

    Better install script

--- frameworks/grabycmd.sh
+++ frameworks/grabycmd.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/Valloric/ycmd.git
+cd ycmd
+git submodule update --init --recursive
+echo 'You now probably want to run something like
+./build.py --all'
